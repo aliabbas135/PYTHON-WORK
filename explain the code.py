fbfad7fe8144008c2ddba96c0b5011bcf3e1bf8e
ALI ABBAS <aaaaleeee135@gmail.com>
2026-09-09 01:53:35 +0500

    Create explain the code.py

---
+++ explain the code.py
@@ -0,0 +1,15 @@
+age = 20
+item = 5
+print("age : ",age)
+print("items : ", item)
+
+age = float(age)
+item = float(item)
+print("age : ",age)
+print("items: ", item)
+output = age = 20
+decimal: item = 5
+float:   age = 20.0
+float:   item = 5.0
+WHAT IT DOES?
+          we were given integers number we have to convert them into float through python programming.as it is written above first they were decimals than after output we changed them into float by adding .0 in the last f them.
