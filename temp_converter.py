b29419a0524b65ce4a20f1aac67e8947f9467e26
ALI ABBAS <aaaaleeee135@gmail.com>
2026-09-09 00:23:38 +0500

    Create temp_converter.py

---
+++ temp_converter.py
@@ -0,0 +1,7 @@
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius} Celsius is equal to {fahrenheit} Fahrenheit")
+
+fahrenheit2 = float(input("Enter temperature in Fahrenheit: "))
+celsius2 = (fahrenheit2 - 32) * 5/9
+print(f"{fahrenheit2} Fahrenheit is equal to {celsius2} Celsius")
